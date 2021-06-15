184e08ff3de7f8c631761b9733777945dd407d57
Caedael <caedael@posteo.de>
2021-06-15 21:28:10 +0200

    added multiple inputfiles

--- script.py
+++ script.py
@@ -2,30 +2,35 @@
 
 import sys, getopt
 
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 def main(argv):
-    inputfile=''
+    inputfiles=[]
     webhookurl=''
     username='File Bot'
     
     try:
-        opts, args = getopt.getopt(argv, "i:w:u:", ["inputfile=", "webhookurl=", "username="])
+        opts, args = getopt.getopt(argv, "hi:w:u:", ["help", "inputfile=", "webhookurl=", "username="])
     except getopt.GetoptError:
         print("input error")
         sys.exit(2)
     for opt, arg in opts:
+        if opt in("-h", "--help"):
+            print("-w; --webhook            Webhook URL")
+            print("-i; --inputfile          Inputfile (one file per inputfile argument)")
+            print("-u; --username           Username")
+            print("-h; --help               this message")
         if opt in("-i", "--inputfile"):
-            inputfile=arg
+            inputfiles.append(arg)
         elif opt in("-w", "--webhook"):
             webhookurl=arg
         elif opt in("-u", "--username"):
             username=arg
 
     webhook=DiscordWebhook(url=webhookurl, username=username)
-    print(inputfile.partition('/')[1])
-    with open(inputfile, "rb") as f:
-        webhook.add_file(file=f.read(), filename=inputfile)
+    for inputfile in inputfiles:
+        with open(inputfile, "rb") as f:
+            webhook.add_file(file=f.read(), filename=inputfile)
 
     response = webhook.execute()
     print(response)
